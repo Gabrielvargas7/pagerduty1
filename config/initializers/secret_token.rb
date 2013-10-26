97cd867a97b3790f9dddf53429c49992f60bb47a
rooms.team@mywebroom.com <rooms.team@mywebroom.com>
2013-10-26 15:04:57 -0700

    first data

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagerduty::Application.config.secret_token = '42dece2dcfeea4038b02b353cc43a235b20ae78dc09bd0c9e1057e1c2dd6acce5c270358be32ca084dabf7813425b9d788bc0bb92dc974a94a307ed0f485b73b'
